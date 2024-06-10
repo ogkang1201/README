badf4061de2cc9db5b1dfbb4cfe2f0529ee67a22
ogkang1201 <147465061+ogkang1201@users.noreply.github.com>
2024-06-10 18:22:55 +0900

    Enable headless mode for matplotlib

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 from cgi import parse_qs
 from template import html
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as mpl
+mpl.use('Agg')
 
 def application(environ, start_response):
     if environ['PATH_INFO'] == './img/graph.png':
